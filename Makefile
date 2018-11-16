3a2c6115ad965ab146df13adc185a26d7214d1ee
andrewkrug <akrug@mozilla.com>
2018-11-16 12:48:13 -0800

    add guardduty plumbing functions

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 ROOT_DIR	:= $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 PARENTDIR       := $(realpath ../)
+AWS_REGION = us-west-2
 S3_BUCKET_NAME  := infosec-public-data
 S3_BUCKET_PATH	:= cf/guardduty-multi-account-manager
 S3_BUCKET_URI	:= s3://$(S3_BUCKET_NAME)/$(S3_BUCKET_PATH)
+
+all:
+	@echo 'Available make targets:'
+	@grep '^[^#[:space:]].*:' Makefile
 
 .PHONY: test
 test:
@@ -18,3 +23,27 @@ cflint: ## Verify the CloudFormation template pass linting tests
 upload-templates:
 	@export AWS_REGION=$(AWS_REGION)
 	aws s3 sync cloudformation/ $(S3_BUCKET_URI) --acl public-read
+
+.PHONY: upload-gd2md-lambda
+upload-gd2md-lambda:
+	@export AWS_REGION=$(AWS_REGION)
+	zip lambda_functions/gd2md.zip lambda_functions/normalization.py
+	aws s3 cp lambda_functions/gd2md.zip s3://infosec-public-data/lambda/gd2md.zip --acl public-read
+	rm lambda_functions/gd2md.zip
+
+.PHONY: upload-plumbing-lambda
+upload-plumbing-lambda:
+	@export AWS_REGION=$(AWS_REGION)
+	zip lambda_functions/plumbing.zip lambda_functions/plumbing.py
+	aws s3 cp lambda_functions/plumbing.zip s3://infosec-public-data/lambda/plumbing.zip --acl public-read
+	rm lambda_functions/plumbing.zip
+
+.PHONY: upload-templates create-stack
+create-stack:
+	@export AWS_REGION=$(AWS_REGION)
+	aws cloudformation create-stack --stack-name guardduty-multi-account-manager --template-url https://s3.amazonaws.com/infosec-public-data/cf/guardduty-multi-account-manager/guardduty-multi-account-manager-parent.yml
+
+.PHONY: upload-templates create-stack
+create-stackset-roles:
+	@export AWS_REGION=$(AWS_REGION)
+	aws cloudformation create-stack --stack-name stackset-administrative-roles --template-url https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/AWSCloudFormationStackSetAdministrationRole.yml
---
+++ lambda_functions/normalization.py
@@ -0,0 +1,66 @@
+import boto3
+import json
+import logging
+
+from datetime import datetime
+from os import getenv
+
+
+logger = logging.getLogger(__name__)
+SNS_OUTPUT_TOPIC_ARN = getenv('SNS_OUTPUT_TOPIC_ARN')
+
+def convert_my_iso_8601(iso_8601):
+    assert iso_8601[-1] == 'Z'
+    iso_8601 = iso_8601[:-1] + '000'
+    iso_8601_dt = datetime.strptime(iso_8601, '%Y-%m-%dT%H:%M:%S.%f')
+    return str(iso_8601_dt)
+
+def send_to_sns(event, sns_client):
+    """Send the transformed message to the SNS topic that outputs to another SQS queue."""
+    return sns_client.publish(
+        TopicArn=SNS_OUTPUT_TOPIC_ARN,
+        Message=json.dumps(event)
+    )
+
+def _get_resource_info(guardduty_event):
+    resource = guardduty_event['detail'].get('resource', {})
+    instance_detail = resource.get('instanceDetails', None)
+    if instance_detail is not None:
+        return instance_detail.get('instanceId')
+    else:
+        return 'guardduty-{account_id}'.format(account_id=event.get('account'))
+
+def transform_event(event):
+    """Take guardDuty SNS notification and turn it into a standard MozDef event."""
+    guardduty_event = json.loads(event['Sns']['Message'])
+
+    mozdef_event = {
+        'timestamp': convert_my_iso_8601(event['Sns'].get('Timestamp')),
+        'hostname': _get_resource_info(guardduty_event),
+        'processname': 'guardduty',
+        'processid': 1337,
+        'severity': 'INFO',
+        'summary': guardduty_event['detail']['description'],
+        'category': guardduty_event['detail']['type'],
+        'source': 'guardduty',
+        'tags': [
+            guardduty_event['detail']['service']['action']['actionType']
+        ],
+        'details': guardduty_event.get('detail')
+    }
+
+    # there is only one 'service', guard duty
+    # rename details.service to details.finding
+    # to make it more descriptive and match aws docs
+    # and avoid schema collisions
+    mozdef_event['details']['finding']=mozdef_event['details'].pop('service')
+
+    return mozdef_event
+
+def handle(event, context):
+    """Basic lambda handler."""
+    sns_client = boto3.client('sns')
+    for record in event.get('Records', []):
+        mozdef_event = transform_event(record)
+        res = send_to_sns(mozdef_event, sns_client)
+    return mozdef_event
